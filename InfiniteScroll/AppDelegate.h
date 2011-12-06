85befd0469450b987bc9d27066b55c8462ec4ca9
cloud <v2.cloud@gmail.com>
2011-12-06 16:05:35 +0800

    Initial Commit

---
+++ InfiniteScroll/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  InfiniteScroll
+//
+//  Created by  on 11-12-6.
+//  Copyright (c) 2011年 DremTop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ InfiniteScroll/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  InfiniteScroll
+//
+//  Created by  on 11-12-6.
+//  Copyright (c) 2011年 DremTop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
